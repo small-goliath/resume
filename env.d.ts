8fbc9012b6c88f838cecbed261579fa63019ee29
small-goliath <meang617@gmail.com>
2025-02-10 11:50:48 +0900

    메타데이터 추가

--- env.d.ts
+++ env.d.ts
@@ -2,26 +2,28 @@
 
 declare namespace NodeJS {
     interface ProcessEnv {
-        readonly NEXT_PUBLIC_PROFILE_NAME: string;
-        readonly NEXT_PUBLIC_PROFILE_EMAIL: string;
-        readonly NEXT_PUBLIC_PROFILE_ADDRESS: string;
-        readonly NEXT_PUBLIC_PROFILE_BIRTHDAY: string;
-        readonly NEXT_PUBLIC_PROFILE_GITHUB: string;
-        readonly NEXT_PUBLIC_PROFILE_TECH_BLOG: string;
-        readonly NEXT_PUBLIC_PROFILE_RESUME_OBJECT_PATH: string;
-        readonly NEXT_PUBLIC_PROFILE_CAREER_OBJECT_PATH: string;
-        readonly NEXT_PUBLIC_PROFILE_BACKGROUND_OBJECT_PATH: string;
-        readonly NEXT_PUBLIC_PROFILE_BACKGROUND_MEDIA_TYPE: string;
-        readonly NEXT_PUBLIC_CDN_BASE_URL: string;
-        readonly NEXT_PUBLIC_EDUCATION: string;
-        readonly NEXT_PUBLIC_SKILLS: string;
-        readonly NEXT_PUBLIC_AWARD: string;
-        readonly NEXT_PUBLIC_VOLUNTEER: string;
-        readonly NEXT_PUBLIC_INTERNATIONAL: string;
-        readonly NEXT_PUBLIC_INTERNSHIP: string;
-        readonly NEXT_PUBLIC_TIMELINE: string;
-        readonly NEXT_PUBLIC_RESEARCH: string;
-      }
+      readonly NEXT_PUBLIC_WEB_URI: string;
+      readonly NEXT_PUBLIC_WEB_DESCRIPTION: string;
+      readonly NEXT_PUBLIC_PROFILE_NAME: string;
+      readonly NEXT_PUBLIC_PROFILE_EMAIL: string;
+      readonly NEXT_PUBLIC_PROFILE_ADDRESS: string;
+      readonly NEXT_PUBLIC_PROFILE_BIRTHDAY: string;
+      readonly NEXT_PUBLIC_PROFILE_GITHUB: string;
+      readonly NEXT_PUBLIC_PROFILE_TECH_BLOG: string;
+      readonly NEXT_PUBLIC_PROFILE_RESUME_OBJECT_PATH: string;
+      readonly NEXT_PUBLIC_PROFILE_CAREER_OBJECT_PATH: string;
+      readonly NEXT_PUBLIC_PROFILE_BACKGROUND_OBJECT_PATH: string;
+      readonly NEXT_PUBLIC_PROFILE_BACKGROUND_MEDIA_TYPE: string;
+      readonly NEXT_PUBLIC_CDN_BASE_URL: string;
+      readonly NEXT_PUBLIC_EDUCATION: string;
+      readonly NEXT_PUBLIC_SKILLS: string;
+      readonly NEXT_PUBLIC_AWARD: string;
+      readonly NEXT_PUBLIC_VOLUNTEER: string;
+      readonly NEXT_PUBLIC_INTERNATIONAL: string;
+      readonly NEXT_PUBLIC_INTERNSHIP: string;
+      readonly NEXT_PUBLIC_TIMELINE: string;
+      readonly NEXT_PUBLIC_RESEARCH: string;
+    }
 }
 
 export { };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,18 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 // TODO: css 적용
 export default function Document() {
+  const userName = `${process.env.NEXT_PUBLIC_PROFILE_NAME}`;
+  const webUri = `${process.env.NEXT_PUBLIC_WEB_URI}`;
+  const webDescription = `${process.env.NEXT_PUBLIC_WEB_DESCRIPTION}`;
+
   return (
     <Html lang="ko">
       <Head>
+        <meta property="og:url" content={webUri} />
+        <meta property="og:title" content={userName} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/profile.png" />
+        <meta property="og:description" content={webDescription} />
         <link rel="stylesheet" href="styles/App.css" />
       </Head>
 
